e4e2f03332eea298de9fa760ef0345435e14b665
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-08-22 16:20:31 +0530

    Part 16
    Setup mvc in asp net core

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -25,6 +25,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,11 +37,8 @@ namespace EmployeeManagement
             }
             
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hosting Environment:" + env.EnvironmentName);
-            });
         }
     }
 }
