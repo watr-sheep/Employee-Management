11f9e010cdb3311bdbc377bc8c1c1358cef72352
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-08-22 16:51:08 +0530

    Part 18
    Model in ASP NET Core MVC

---
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Email = "mary@gmail.com", Department = "HR"},
+                new Employee() { Id = 2, Name = "John", Email = "john@gmail.com", Department = "Dev"},
+                new Employee() { Id = 3, Name = "Sam", Email = "sam@gmail.com", Department = "QE"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
