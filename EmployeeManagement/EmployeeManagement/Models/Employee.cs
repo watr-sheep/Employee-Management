ceb23cc30a9e588ab170bf39dec0621c7764f167
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-08-28 16:09:57 +0530

    Keeping domain models and database schema in sync in asp net core
    Part 52

--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -17,5 +17,6 @@ namespace EmployeeManagement.Models
         public string Email { get; set; }
         [Required]
         public Dept? Department { get; set; }
+        public string PhotoPath { get; set; }
     }
 }
