81b2a6177a94235907b58b18ea48300c93db1353
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-09-03 19:52:25 +0530

    Centralised 404 error handling in ASP NET Core
    Part 58

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -39,6 +39,10 @@ namespace EmployeeManagement
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
             
             app.UseStaticFiles();
             //app.UseMvcWithDefaultRoute();
