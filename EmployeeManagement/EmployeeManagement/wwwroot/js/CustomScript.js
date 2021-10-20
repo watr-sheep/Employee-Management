ca5e1708db2e01575280b2f735d67f4d8984af45
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-10-20 08:45:48 +0530

    ASP NET Core delete confirmation
    87

--- EmployeeManagement/EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,1 +1,13 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmedDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmedDeleteSpan).show();
+    }
+    else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmedDeleteSpan).hide();
+    }
+}
