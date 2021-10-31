e52170bb4349af599487a2c753a8919a1a6b1058
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-10-31 21:31:08 +0530

    Authorization in views in asp net core mvc
    Part 96

--- EmployeeManagement/EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/AdministrationController.cs
@@ -138,6 +138,7 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpGet]
+        [Authorize(Policy = "EditRolePolicy")]
         public async Task<IActionResult> EditRole(string id)
         {
             var role = await roleManager.FindByIdAsync(id);
@@ -166,6 +167,7 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
+        [Authorize(Policy = "EditRolePolicy")]
         public async Task<IActionResult> EditRole(EditRoleViewModel model)
         {
             var role = await roleManager.FindByIdAsync(model.Id);
