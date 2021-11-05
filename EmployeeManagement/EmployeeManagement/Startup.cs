9d29584cb11194c46f62e102296a495abf4274d0
sandun_abey <57134382+watr-sheep@users.noreply.github.com>
2021-11-05 17:38:22 +0530

    Multiple custom authorization handlers for a requirement in asp net core

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -64,6 +64,8 @@ namespace EmployeeManagement
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
 
             services.AddSingleton<IAuthorizationHandler, CanEditOnlyOtherAdminRolesAndClaimsHandler>();
+            services.AddSingleton<IAuthorizationHandler, SuperAdminHandler>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
